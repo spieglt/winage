f61cc5d07dcbf6f971282d1e032de088b07ddfe9
Theron <tspiegl@gmail.com>
2021-09-14 22:13:46 -0500

    oops

--- src/identity.rs
+++ src/identity.rs
@@ -8,8 +8,8 @@ pub fn generate(identity_path: &str) -> Result<()> {
     let pk = sk.to_public();
     let mut out_file = File::create(identity_path)?;
     // TODO: internationalization
-    writeln!(out_file, "# created: {}", chrono::Local::now().to_rfc3339_opts(chrono::SecondsFormat::Secs, true));
-    writeln!(out_file, "# public key: {}", pk);
-    writeln!(out_file, "# created: {}", sk.to_string().expose_secret());
+    writeln!(out_file, "# created: {}", chrono::Local::now().to_rfc3339_opts(chrono::SecondsFormat::Secs, true))?;
+    writeln!(out_file, "# public key: {}", pk)?;
+    writeln!(out_file, "{}", sk.to_string().expose_secret())?;
     Ok(())
 }
